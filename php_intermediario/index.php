3ff6021c3926eb76964f05a87a7a5e35b634cc68
Guilherme Alonso <alonsogui011@gmail.com>
2022-09-12 11:25:38 -0300

    example of sleep e die

--- php_intermediario/index.php
+++ php_intermediario/index.php
@@ -8,3 +8,17 @@ echo "<hr>";
 echo "<h2><a style='text-decoration: none;' href='http://localhost/php_basico_nazarena/php_intermediario/array-multi.php'>ARRA's MULTIDIMENSIONAIS</a></h2>";
 
 echo "<hr>";
+
+// sleep -> durma o código por x segundos.
+// die -> "matar" o código
+
+$nome = "guilherme";
+
+if($nome == 'joao'){
+    echo 'tudo certo';
+} else {
+    die("o script parou de ser executado!");
+}
+
+
+?>
