23740eb49699b99b8d295097899f2c4a0fe49ba6
Guilherme Alonso <alonsogui011@gmail.com>
2022-09-12 10:55:57 -0300

    formas diferentes ter fazer um array

--- php_intermediario/array-multi.php
+++ php_intermediario/array-multi.php
@@ -1,5 +1,17 @@
 <?php
 echo "<h3>ARRAY MULTIDIMENSIONAL</h3>";
+
+// array single
+$arr = ['guilherme', 'vinicius'];
+$arr = array('guilherme', 'chave2' => 'vinicius');
+
+$arr[0] = 'guilherme';
+$arr[] = 'joao';
+
+
+// array multidimensionais
+$arr2 = array(array('Guilherme', 'Vinicius'), array(25,13));
+echo $arr2[1][0];
 ?>
 <br>
 <a style="text-decoration: none;" href="http://localhost/php_basico_nazarena/php_intermediario/">VOLTAR</a>
